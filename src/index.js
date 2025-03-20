890d1d4aa26d528f4663fb9a65e1310d880582f8
edel <edel.omondi@student.moringaschool.com>
2025-03-20 20:14:48 +0300

    completed 1 lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  document.addEventListener("DOMContentLoaded", () => {
+    const form = document.querySelector("#create-task-form");
+    const input = document.querySelector("#new-task-description");
+    const taskList = document.querySelector("#tasks"); 
+
+    form.addEventListener("submit", (event) => {
+        event.preventDefault();
+
+        const taskText = input.value.trim();
+        if (taskText) {
+            const taskItem = document.createElement("li");
+            taskItem.textContent = taskText;
+            taskList.appendChild(taskItem);
+            input.value = ""; 
+        }
+    });
 });
+
+
